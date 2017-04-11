731c2f7a41954542344f24fa2212242cd0f4f274
hieuxinhe94 <hieu.e.content@gmail.com>
2017-04-12 06:52:44 +0700

    hola

--- System/FacebookSetup.aspx.cs
+++ System/FacebookSetup.aspx.cs
@@ -10,21 +10,25 @@ public partial class System_FacebookSetup : System.Web.UI.Page
 {
     public DataTable objTblFbPost = new DataTable();
     FacebookApi api = new FacebookApi();
+    FbPosts objfb_post = new FbPosts();
     protected void Page_Load(object sender, EventArgs e)
     {
         if(!IsPostBack)
         {
-         
-          // 
+            objTblFbPost = objfb_post.getData();
+            //count like , count comment , count xxx
         }
 
     }
     protected void btnRefresh_Click(object sender, EventArgs e)
     {
-            
+        api.refresh();
+        Response.Redirect(Request.RawUrl);
     }
     protected void btnUpdate_Click(object sender, EventArgs e)
     {
-        api.saveToDb();
+        objfb_post.delAllData();
+        api.saveAllToDb();
+        Response.Redirect(Request.RawUrl);
     }
 }
