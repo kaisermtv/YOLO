4aea03784cb5b98caba415d25891e4a30dcbf48d
kaisermtv <kaisermtv@yahoo.com>
2017-04-17 09:31:28 +0700

    ể

--- FrontEnd/Pages/News.aspx.cs
+++ FrontEnd/Pages/News.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,8 +8,28 @@ using System.Web.UI.WebControls;
 
 public partial class FrontEnd_Pages_News : System.Web.UI.Page
 {
+    #region declare
+    private DataNews objNews = new DataNews();
+    private DataNewsGroup objNewsGroup = new DataNewsGroup();
+
+    public int itemId = 0;
+    #endregion
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        try
+        {
+            this.itemId = int.Parse(Request["id"].ToString());
+        }
+        catch { }
 
+        if (!Page.IsPostBack)
+        {
+            DataTable objData = objNews.getDataTop(10, itemId);
+
+            dtlNews.DataSource = objData.DefaultView;
+            dtlNews.DataBind();
+
+        }
     }
 }
