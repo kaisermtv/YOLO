57c1a1e3584d3440a0cfb2704faca40d4f94c836
hieuxinhe94 <hieu.e.content@gmail.com>
2017-04-18 16:28:49 +0700

    format code

--- App_Code/Facebook/FacebookApi.cs
+++ App_Code/Facebook/FacebookApi.cs
@@ -16,25 +16,17 @@ public class FacebookApi : DataClass
 
     private static String fields = "full_picture,picture,link,message,likes{name,id},comments,created_time";
     private static string fields_album = "photos{images,name,link,likes.limit(0).summary(true), comments.limit(0).summary(true),created_time}";          // chú ý không dùng created_time trong comments
-   // private static String access_token = @"EAAbazmfYd8gBAEeLHQuUZC61YRka9XEOU4eUOtuSmFaZAVF1i6vDuUQk752xfZANGpZCJjOtqm0ZBR91ZCH6zR64QvNsfYxcyRJaQTIXrF1C6Fnfxe4gfLmxMTmzmtsSLJyfZBPPcHx6o9wSxgyeOTdWF2EramU6S4ZD";
-    private static string site_id = "ngheansunshine";
+    private static string site_id = "ngheansunshine";  
     private static string album_id = "790172047808833";
-    //  private string url = @"https://graph.facebook.com/"+site_id+"/posts?fields=" + fields + "&limit=" + limit + "&access_token=" +access_token + "";
-    //   private string url_album = @"https://graph.facebook.com/" + album_id + "?fields=" + fields_album + "&limit=" + 30 + "&access_token=" + access_token + "";
     private static string key_db = "Facebook_Token";
     private static string album_key_id = "yolodamchiase";
-    //  private string url2 = @"https://graph.facebook.com/790172047808833?fields=photos{link,likes{id,username},name,picture,comments{message,from},page_story_id}&limit=5&access_token=https://graph.facebook.com/ngheansunshine/posts?fields=full_picture,picture,link,message,created_time&limit=5&access_token=EAAbazmfYd8gBAEeLHQuUZC61YRka9XEOU4eUOtuSmFaZAVF1i6vDuUQk752xfZANGpZCJjOtqm0ZBR91ZCH6zR64QvNsfYxcyRJaQTIXrF1C6Fnfxe4gfLmxMTmzmtsSLJyfZBPPcHx6o9wSxgyeOTdWF2EramU6S4ZD";
-    /*
-        Thực hiện lấy dử liệu và đưa vào database
-        Cho phép load lại dử liệu / bài viết đả xóa ở fb
-     
-     * * */
+    
 	public FacebookApi()
 	{
 	}
-    public int refressPhotoPost(int limits = 30)   // lấy 30 bài mới nhất  "refresh"
+    public int refressPhotoPost(int limits = 10)   // lấy 30 bài mới nhất  "refresh"
     {
-        string json = getJsonString(2, limits);              // kiểu số 2          
+        string json = getJsonString(2, 25,"");              // kiểu số 2          
         if (json == null || json.Trim() == "") return 0;
         FbPhotoAlbum lPpost = new FbPhotoAlbum("", "", "", "","", new comments(), new likes());
         try
@@ -50,7 +42,6 @@ public class FacebookApi : DataClass
             Debug.WriteLine("=[EURROR] CANNOT REFRESH THE POST of Article FROM FB:  " + e.GetBaseException());
             return 0;
         }
-       
         return 1;
     }
 
@@ -64,7 +55,6 @@ public class FacebookApi : DataClass
             case 1: {
                 FbPosts lpost = new FbPosts("", "", "", "", "", "", new List<comments>(), new List<likes>());
                 lpost.data = parseJsonToPosts(json);
-
                 foreach (var item in lpost.data)
                 {
                     insertValueWithCheckExist(item.id.Split('_')[1].ToString(), item.message, item.full_picture, item.picture, item.link, item.created_time, item.comments.Count.ToString(), item.likes.Count.ToString());
@@ -80,7 +70,6 @@ public class FacebookApi : DataClass
                     {
                         insertPhotoPostValueWithCheckExist(item.id, item.name, item.picture, item.link, item.comments.summary, item.likes.summary,item.create_time);
                     }
-
                     break;
                 }
             default: break;
@@ -99,7 +88,6 @@ public class FacebookApi : DataClass
     public List<FbPhotoAlbum> parseJsonToPhotoPosts(string json)
     {
         string [] json_arr = new string [10];
-        
         FbPhotoAlbum lphoto_post = new FbPhotoAlbum("", "", "", "", "", new comments(), new likes());
         lphoto_post.data = new List<FbPhotoAlbum>();
         try
@@ -121,7 +109,7 @@ public class FacebookApi : DataClass
                 next_data = struff.paging;
                 }
                 catch { break; }
-            }
+            } 
 
             foreach (string s in json_arr)
             {
@@ -164,7 +152,7 @@ public class FacebookApi : DataClass
                         string likes = element["likes"].ToString();
                         dynamic struff_likes = JsonConvert.DeserializeObject(likes);
                         ll.summary = struff_likes["summary"]["total_count"].ToString() == null ? " 0" : struff_likes["summary"]["total_count"].ToString();
-                            Debug.WriteLine(ll.summary);
+                        Debug.WriteLine(ll.summary);
                     }
                  
                     lphoto_post.data.Add(
@@ -176,11 +164,8 @@ public class FacebookApi : DataClass
                         element["create_time"] == null ? "" : element["create_time"].ToString(),
                         lc == null ? new comments() : lc,
                         ll == null ? new likes() : ll
-                     
                        ));
-                  
                 }
-           // }
             if (lphoto_post.data.Count < 1) return new List<FbPhotoAlbum>();
         }
         catch (Exception e)
@@ -253,7 +238,7 @@ public class FacebookApi : DataClass
     #endregion
 
     #region refreshPost                        || Save getJson -> Parse Object -> update for SqlServer || run in first time
-    public int refreshPost(int limits=30)
+    public int refreshPost(int limits=10)
     {
         string json = getJsonString(1, limits);                        // lấy 30 bài mới nhất thôi
         if (json == null || json.Trim() == "") return 0;
@@ -274,72 +259,6 @@ public class FacebookApi : DataClass
     }
     #endregion
     // facebook auto change token by time
-    #region setNewAccessToken()                 || try to set new token when not valin
-    public int setNewAccessToken(string new_token)
-    {
-        // hoặc nhờ người quản trị nhập vào ... 
-        // lấy từ 1 dòng nào đó của cơ sở dữ liệu 
-        // và do người dùng nhập vào
-        DataSetting setting = new DataSetting();
-        if (setting.setValue(key_db, new_token))
-        {
-            Debug.WriteLine("=[SUCCESS] UPDATE NEW TOKEN TO DATABASE IN SETTING SUCCESS " + new_token);
-            return 1;
-        }
-        Debug.WriteLine("=[EURROR] CANNOT UPDATE TO SETTING FB TOKEN WITH KEY :  " + key_db );
-        return 0;
-    }
-    #endregion
-
-    #region getNewAccessToken()                 || try to get new token when not valin
-    public string getNewAccessToken()
-    {
-        DataSetting setting = new DataSetting();
-              string token = " ";
-            token= setting.getValue(key_db);
-        // lấy từ 1 dòng nào đó của cơ sở dữ liệu 
-        // và do người dùng nhập vào
-         if(token!= null || token.Trim() !=""){
-            Debug.WriteLine("=[SUCCESS] GET ACCESS TOKEN FROM DATATABLE  " + token);
-           }
-            Debug.WriteLine("! [ERROR] GET ACCESS TOKEN FROM DATATABLE  IS NULL OR  " + token);
-            return token;
-     }
-    #endregion
-
-    #region insertMutiLineValue()           ||Cấu hình bảng || Đưa vào cơ sở dử liệu
-    public int insertMultiValue(string value)
-    {
-        try {
-            SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["TVSConnect"].ConnectionString);
-            conn.Open();
-            SqlCommand sqlCommand = new SqlCommand();
-            sqlCommand.CommandText = @" IF (OBJECT_ID('tblFacebookPost') is not null )
-                                            BEGIN  
-                                                           INSERT INTO tblFacebookPost (id,message,full_picture,picture,link,created_time,comments,likes) VALUES         
-                                                           (
-                                                            " + value + @"
-                                                            )   ;  
-                                            END 
-                                        ELSE BEGIN    
-                                            CREATE  TABLE tblFacebookPost (PostId int identity(1,1)  not null primary key,id char(30) ,message nvarchar(max) ,full_picture char(450) ,picture char(450) ,link char(500),created_time char(30) ,comments nvarchar(10),likes nvarchar(10), time_sync datetime default getdate()  )   ;                           
-                                             
-                                              END   
-                                                    ";
-            sqlCommand.ExecuteNonQuery();
-            conn.Close();
-            conn.Dispose();
-            return 1;
-        }
-        catch(Exception e)
-        {
-            Debug.WriteLine("[ERROR] : " + e.GetBaseException());
-            return -1;
-        }
-
-    }
-#endregion
-
     #region insertValueWithCheckExist()           | Đưa vào cơ sở dử liệu , kiểm tra xem bài viết tồn tại chưa
     public int insertValueWithCheckExist(string id, string message, string full_picure, string picture, string link, string create_time, string lc_count ,string ll_count)
     {
@@ -421,16 +340,15 @@ public class FacebookApi : DataClass
     #region getJsonString                       
     public string getJsonString( int type ,int limits = 50,string urls= "" )
     {
-       
         string url = "";
-           string  access_token = new DataSetting().getValue(key_db).Trim();
+        string  access_token = new DataSetting().getValue(key_db).Trim();
         //có thể lấy album_id hoặc site_id tại csdl
         if (access_token == "") return null ;
         switch(type)
         {
             case 1:
                 {
-                    url = @"https://graph.facebook.com/" + site_id + "/posts?fields=" + fields + "&limit=" + limits + "&access_token=" + access_token + "";  // mặc định là lấy bài viết // normarl
+                    url = @"https://graph.facebook.com/" + new DataSetting().getValue("PAGE_ID") + "/posts?fields=" + fields + "&limit=" + limits + "&access_token=" + access_token + "";  // mặc định là lấy bài viết // normarl
                     break;
                 }
             case 2:
@@ -447,14 +365,11 @@ public class FacebookApi : DataClass
         }       
         try
         {
-          
-
              var json = "";
              var client = new WebClient();
              client.Headers.Add("Content-Type", "application/json");
              json = client.DownloadString(url);
-            
-            Debug.WriteLine("====== GET JSON STRING  " + json);
+             Debug.WriteLine("====== GET JSON STRING  " + json);
                 return json;
          }
         catch(Exception e)
@@ -465,11 +380,9 @@ public class FacebookApi : DataClass
     }
     #endregion
 
-
     #region convert  Name  
     public string convertName(string str)
     {
-
         string[] tmpHoten = str.ToString().Split('\n');
         string result = "";
         for (int i = 0; i < tmpHoten.Length && i<3; i++)
@@ -477,10 +390,7 @@ public class FacebookApi : DataClass
             result += tmpHoten[i].ToString() + "|";
         }
         Debug.WriteLine(result);
-       
-
           return result;
     }
     #endregion
-
 }
