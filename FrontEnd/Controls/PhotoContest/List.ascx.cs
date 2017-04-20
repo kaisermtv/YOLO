d8c678d0eefe5cfb2b69155e5275b52723eb8138
lexkhero <lexkhero5590@gmail.com>
2017-04-20 16:20:02 +0700

    fsdfdsf

---
+++ FrontEnd/Controls/PhotoContest/List.ascx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class FrontEnd_Controls_PhotoContest_List : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    public void BindData(DataTable dt)
+    {
+        if (dt != null && dt.Rows.Count > 0)
+        {
+            ListContest.DataSource = dt;
+            ListContest.DataBind();
+        }
+        else this.Visible = false;
+
+    }
+}
