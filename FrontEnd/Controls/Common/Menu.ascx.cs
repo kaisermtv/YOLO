b9bb1fd1d271ba6f90bc808fc8f19657b65ede77
kaisermtv <kaisermtv@yahoo.com>
2017-04-27 10:01:24 +0700

    vdsv

--- FrontEnd/Controls/Common/Menu.ascx.cs
+++ FrontEnd/Controls/Common/Menu.ascx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -28,6 +29,30 @@ public partial class FrontEnd_Controls_Common_Menu : System.Web.UI.UserControl
         dtlData.DataBind();
 
         index = 1;
+    }
+    #endregion
+
+    #region getActive(string link)
+    public string getActive(string link)
+    {
+      //  link = link.ToLower();
+
+      //  if (link == "/" || link == "/home" || link == "/trang-chu" || Regex.IsMatch(link, "^/home/.*") || Regex.IsMatch(link, "^/trang-chu/.*"))
+      //  {
+
+      //  } 
+
+
+      //  if (link == "/tin-tuc") return "class=\"active\"";
+
+      //  Match m = Regex.Match(link, "-cat[\\d]+$", RegexOptions.IgnoreCase);
+      //if (m.Success)
+      //   Console.WriteLine("Found '{0}' at position {1}.", m.Value, m.Index);
+
+
+        //Regex.IsMatch(link,".*-cat[\\d]",
+
+        return "";
     }
     #endregion
 }
